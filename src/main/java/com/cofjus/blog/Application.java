b8095c3531a5bc3ac4772c7d609454f0b19b8e36
CofJu <1666749261@qq.com>
2021-09-30 20:43:12 +0800

    initial

---
+++ src/main/java/com/cofjus/blog/Application.java
@@ -0,0 +1,16 @@
+package com.cofjus.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Rui
+ * @Date 2021/9/30 15:41
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String... args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
